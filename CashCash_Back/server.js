735b14cdbdc8c8cf49cf08a79d829c3d84e71045
FilleDeZeus <enide26@hotmail.fr>
2024-03-18 11:22:02 +0100

    creation backend avec server.js

---
+++ CashCash_Back/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+const port = 3000;
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'enidedjender',
+    password: 'enide',
+    database: 'cashcash'
+});
+
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connecté à la base de données MySQL');
+});
+
+app.get('/utilisateurs', (req, res) => {
+    db.query('SELECT * FROM Utilisateur', (err, result) => {
+        if (err) {
+            res.status(500).send('Erreur lors de la récupération des utilisateurs');
+            } else {
+            res.json(result);
+        }
+    });
+});
+
+
+app.listen(port, () => {
+    console.log(`Serveur backend démarré sur http://localhost:${port}`);
+});
